8c5384e7dc27f50868526394607afde8c8729320
wgmv <wgmv@users.noreply.github.com>
2021-01-29 22:16:17 +0000

    Fix styling

--- src/Tag.php
+++ src/Tag.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Collection as DbCollection;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
-use Spatie\Translatable\HasTranslations;
 
 class Tag extends Model implements Sortable
 {
